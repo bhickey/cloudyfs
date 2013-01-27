d8dc92fa981ab85e5e37c1ad02957144dce35f05
Brendan Hickey <brendan@bhickey.net>
2013-01-27 06:35:06 +0000

    REPORT ALL THE WEATHER

--- System/CloudyFS/Weather.hs
+++ System/CloudyFS/Weather.hs
@@ -1,6 +1,19 @@
 module System.CloudyFS.Weather where
 
+import Data.Maybe (fromJust)
+
 import Data.ByteString.Char8
+import Data.Time.Clock
+import Data.Time.LocalTime
+
+import Network.HTTP
+import Text.HTML.TagSoup
+import qualified Data.Time.RFC2822 as RFC2822
+
+data Weather = Weather {
+  time :: UTCTime,
+  conditions :: String
+} deriving (Show)
 
 getWeatherFor :: [String] -> IO ByteString
 getWeatherFor _ = do
@@ -8,3 +21,16 @@ getWeatherFor _ = do
     return $ pack (url ++ "\n")
 --    do res <- simpleHTTP (getRequest url) >>= getResponseBody
 --       return $ pack res
+
+getURL :: String -> IO String
+getURL x = getResponseBody =<< simpleHTTP (getRequest x)
+
+fetchWeather :: String -> IO Weather
+fetchWeather station = do
+    tags <- fmap parseTags $ getURL url
+    return $ Weather (getDate tags) (getConditions tags)
+  where
+    url = "http://w1.weather.gov/xml/current_obs/" ++ station ++ ".xml"
+    getText tag blob = fromTagText $ sections (~== tag) blob !! 0 !! 1
+    getDate t = zonedTimeToUTC $ fromJust $ RFC2822.readRFC2822 (getText "<observation_time_rfc822>" t)
+    getConditions = (getText "<weather>")
